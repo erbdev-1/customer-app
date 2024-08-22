c5e4b5dd41e11fb70f1ffb0bbe10c288e5bc2e24
erbdev <erbdev@hotmail.com>
2024-08-22 01:09:58 +0100

    first commit

---
+++ src/components/CustomerItem/CustomerItem.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import "./CustomerItem.css";
+
+const DeleteButton = styled.button`
+  background-color: red;
+  padding: 8px;
+  transition: opacity 0.5s ease;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const CustomerItem = ({ customer, handleDelete }) => {
+  return (
+    <li className="customer-item">
+      <div className="customer-info">
+        <img
+          src="https://i.pravatar.cc/300"
+          alt=""
+          className="customer-avatar"
+        />
+        <span className="customer-name">{customer.customerName}</span>
+      </div>
+      <DeleteButton
+        className="delete-button"
+        onClick={() => handleDelete(customer)}
+      >
+        <i className="bi bi-trash3"></i>
+      </DeleteButton>
+    </li>
+  );
+};
+
+export default CustomerItem;
